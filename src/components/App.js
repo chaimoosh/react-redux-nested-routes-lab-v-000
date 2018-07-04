b48cddc6392df08a47ba1f0f1a3f396e8a39fcb6
Chaim Reisman <chaim@Chaims-MacBook-Pro.local>
2018-07-03 21:49:08 -0400

    Done.

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { BrowserRouter as Router, NavLink, Route } from 'react-router-dom';
 import PetsPage from '../containers/PetsPage';
-
-const App = (props) => 
+import PetsShow from '../containers/PetsShow'
+import PetsNew from '../containers/PetsNew'
+const App = (props) =>
   <Router>
     <div>
       <div style={{ borderBottom: '2px solid black', paddingBottom: '10px', marginBottom: '12px' }}>
@@ -11,6 +12,8 @@ const App = (props) =>
       </div>
       <Route exact path="/" render={() => <h3>Welcome to the Pets List App</h3>} />
       <Route path="/pets" component={PetsPage} />
+      <Route exact path={`pets/new`} component={PetsNew} />
+      <Route path={`pets/:petId`} component={PetsShow}/>
     </div>
   </Router>;
 
